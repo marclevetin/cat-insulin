1148ac82b350f694c11d27e74c2f2d9b713a2794
marclevetin <marc@derbyhat.org>
2017-12-26 23:39:00 -0500

    got index handlebars template to render.  Had to temporarily comment out the links to the model

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Opening requirements and dependencies
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
@@ -12,10 +12,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const handlebars = require("express-handlebars");
 
-app.engine("handlebars, handlebars({ defaultLayout: "main" }));)
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get('/', (req, res)=>{
+  res.render('index',{});
+  console.log('hi mom');
+})
 
 // routes
 const routes = require('./controllers/catInsulin.js')
+
+// app.use("/", routes);
 
 app.listen(port, ()=>console.log(`listening on ${port}`))
